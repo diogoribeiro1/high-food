af3d4c9ed0a0b7d2da01610a178019586dc9e055
TheVinny <vinny.vip07@gmail.com>
2023-02-19 23:50:08 -0300

    fix: changes users controller routes

--- src/main/java/com/devus/highfood/modules/users/controller/UserController.java
+++ src/main/java/com/devus/highfood/modules/users/controller/UserController.java
@@ -19,19 +19,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
+    @GetMapping("/user")
     public @ResponseBody List<UserEntity> getUsers(){
         return userService.getAll();
     }
 
-    @PostMapping("/user/register")
+    @PostMapping("/user")
     public UserEntity register(@RequestBody UserEntity userEntity){
         userEntity.setRole(Role.USER);
        var userPayload =  userService.create(userEntity);
       return userPayload;  
     }
 
-    @PostMapping("/admin/register")
+    @PostMapping("/user/admin")
     public UserEntity registerAdmin(@RequestBody UserEntity userEntity){
         userEntity.setRole(Role.ADMIN);
        var userPayload =  userService.create(userEntity);
